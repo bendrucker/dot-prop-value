94f37cf3b6210b5a4e3681e5bfc16bc5628c70c0
Ben Drucker <bvdrucker@gmail.com>
2020-08-18 14:48:47 -0700

    standard --fix

--- test.js
+++ test.js
@@ -7,13 +7,13 @@ test('get', function (t) {
   t.equal(dot.get('foo'), 'foo')
   t.equal(dot.get('foo', ''), 'foo')
   t.notOk(dot.get('foo', 'bar'))
-  t.equal(dot.get({foo: 'bar'}, 'foo'), 'bar')
+  t.equal(dot.get({ foo: 'bar' }, 'foo'), 'bar')
   t.end()
 })
 
 test('set', function (t) {
   t.equal(dot.set('foo', '', 'baz'), 'baz')
-  t.deepEqual(dot.set('foo', 'bar', 'baz'), {bar: 'baz'})
-  t.deepEqual(dot.set({}, 'foo', 'bar'), {foo: 'bar'})
+  t.deepEqual(dot.set('foo', 'bar', 'baz'), { bar: 'baz' })
+  t.deepEqual(dot.set({}, 'foo', 'bar'), { foo: 'bar' })
   t.end()
 })
